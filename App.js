fe04fdeb7f71e1de100e44c4328cda993c67284d
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-04-25 02:39:40 +0530

    removind previous redux

--- App.js
+++ App.js
@@ -1,31 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
 
-import Home from "./screens/Home.js";
 import TabNavigator from "./navigations/TabNavigator";
-
-import { initStore } from "./redux/store";
-import { Provider } from "react-redux";
-
-const store = initStore();
 
 class App extends React.Component {
   render() {
-    return (
-      <Provider store={store}>
-        <TabNavigator />
-      </Provider>
-    );
+    return <TabNavigator />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default App;
--- screens/Home.js
+++ screens/Home.js
@@ -2,10 +2,6 @@ import React from "react";
 import { Text, View, Button } from "react-native";
 import styles from "../styles.js";
 
-import { TEST_ACTION } from "../redux/actions/index.actions";
-
-import { connect } from "react-redux";
-
 class Home extends React.Component {
   constructor() {
     super();
@@ -29,7 +25,7 @@ class Home extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>This is the counter? {this.props.counter}</Text>
+        <Text>This is the counter? {this.state.count}</Text>
         <Button
           title="Add"
           onPress={() => {
@@ -47,14 +43,4 @@ class Home extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    counter: state.counter,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => ({
-  dispatch,
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
