3a08da552d3b84564ba0ebfd2db3b1ba8882c92a
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-04-24 19:46:48 +0530

    changed activity.js

--- screens/Activity.js
+++ screens/Activity.js
@@ -5,7 +5,7 @@ export default class Activity extends React.Component {
   render() {
     return (
       <View>
-        <Text> Activity </Text>
+        <Text> Activity Screen </Text>
       </View>
     );
   }
