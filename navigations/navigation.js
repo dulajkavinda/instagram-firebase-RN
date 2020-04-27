a438a1a48afd07ba936cf3ba02c245d05f0ba409
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-04-27 08:40:21 +0530

    added user state to navigation

--- navigations/navigation.js
+++ navigations/navigation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { useSelector } from "react-redux";
 
 import Loading from "../screens/Loading";
 
@@ -7,8 +8,8 @@ import TabNavigator from "./TabNavigator";
 import AuthNavigator from "./AuthNavigator";
 
 export default () => {
+  const user = useSelector((state) => state.user.uid);
   const [isLoading, setIsLoading] = React.useState(true);
-  const [user, setUser] = React.useState(null);
 
   React.useEffect(() => {
     setTimeout(() => {
