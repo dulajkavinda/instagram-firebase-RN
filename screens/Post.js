8470141215f192f172bea832197e2dc2e583e366
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-04-29 03:32:10 +0530

    updateDescription reducer

--- screens/Post.js
+++ screens/Post.js
@@ -1,12 +1,35 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TextInput } from "react-native";
 
-export default class Post extends React.Component {
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { updateDecription } from "../redux/actions/post";
+
+import styles from "../styles";
+
+class Post extends React.Component {
   render() {
     return (
-      <View>
-        <Text> Upload Screen </Text>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.border}
+          value={this.props.post.description}
+          onChangeText={(input) => this.props.updateDecription(input)}
+          placeholder="Description"
+        />
       </View>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    post: state.post,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ updateDecription }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post);
