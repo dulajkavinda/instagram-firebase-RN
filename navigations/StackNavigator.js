99d533c78b48a8ecbef3a81d3cff905bb969d5d3
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-04-29 05:24:22 +0530

    added stack to tab navigator

---
+++ navigations/StackNavigator.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+import { createStackNavigator } from "@react-navigation/stack";
+
+import TabNavigator from "../navigations/TabNavigator";
+
+const StackTab = createStackNavigator();
+
+const StackTabScreen = () => (
+  <StackTab.Navigator>
+    <StackTab.Screen
+      name="🔥INSTAGRAM 🔥"
+      component={TabNavigator}
+      options={{ headerTitleAlign: "center" }}
+    />
+  </StackTab.Navigator>
+);
+
+export default StackTabScreen;
