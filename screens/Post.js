b42f5f40a5059bee7a906ae40806d6aaaf44fbf0
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-04-29 03:41:43 +0530

    image uploaded

--- screens/Post.js
+++ screens/Post.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, TextInput } from "react-native";
+import { Text, View, TextInput, Image, TouchableOpacity } from "react-native";
 
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
@@ -11,12 +11,25 @@ class Post extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Image
+          style={styles.postPhoto}
+          source={{
+            uri:
+              "https://firebasestorage.googleapis.com/v0/b/instagram-rn-fca81.appspot.com/o/galaxy.jpg?alt=media&token=2a60bd49-60ba-4f36-ab00-fa5c42136614",
+          }}
+        />
         <TextInput
           style={styles.border}
           value={this.props.post.description}
           onChangeText={(input) => this.props.updateDecription(input)}
           placeholder="Description"
         />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => this.props.navigation.navigate("Signup")}
+        >
+          <Text>Post</Text>
+        </TouchableOpacity>
       </View>
     );
   }
