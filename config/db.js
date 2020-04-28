534381398e0c4c7c253a2ec880c7a90a805e23a8
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-04-28 13:02:32 +0530

    created database file

---
+++ config/db.js
@@ -0,0 +1,9 @@
+// මේ file එකෙන වෙන්නනේ firebase එකේ තියන database
+// එක export කරන එක
+// එතකොට මේ හුත්ත හැම තැනම type කරන්න ඕනේ නෑ.
+
+import firebase from "firebase";
+
+const db = firebase.firestore();
+
+export default db;
