48f9dee4525a0b9349b44726192d16078349b5ef
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-05-04 19:49:39 +0530

    signup action types

--- redux/actions/actionTypes.js
+++ redux/actions/actionTypes.js
@@ -1,10 +1,11 @@
+// signup වලට ඕනේ action types
 export const UPDATE_EMAIL = "UPDATE_EMAIL";
 export const UPDATE_PASSWORD = "UPDATE_PASSWORD";
 export const UPDATE_USERNAME = "UPDATE_USERNAME";
 export const UPDATE_BIO = "UPDATE_BIO";
+export const SIGNUP = "SIGNUP";
 
 export const SIGNIN = "SIGNIN";
-export const SIGNUP = "SIGNUP";
 export const USER_STATUS = "USER_STATUS";
 
 export const UPDATE_DESCRIPTION = "UPDATE_DESCRIPTION";
