fee80ead29aed1e2a8d2d702260d3a2b27f04e4d
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-05-04 23:16:05 +0530

    middleware fixed

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunkMiddleware from "redux-thunk";
 import logger from "redux-logger";
+
 const middleware = applyMiddleware(thunkMiddleware, logger);
 const store = createStore(reducer, middleware);
 import firebaseConfig from "./config/firabase";
