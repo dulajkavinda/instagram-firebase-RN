553c95f62aa93a6071db33ba1002344a442f2763
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-05-04 19:48:19 +0530

    counter acion types removed

--- redux/actions/actionTypes.js
+++ redux/actions/actionTypes.js
@@ -1,6 +1,3 @@
-export const ADD = "ADD";
-export const SUBTRACT = "SUBTRACT";
-
 export const UPDATE_EMAIL = "UPDATE_EMAIL";
 export const UPDATE_PASSWORD = "UPDATE_PASSWORD";
 export const UPDATE_USERNAME = "UPDATE_USERNAME";
